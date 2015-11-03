6133e61d224cee133b3c7572d55c32afe5789b18
Mike Binns <TheFirstAvenger@users.noreply.github.com>
2015-11-03 14:52:26 -0500

    Swapped to/from

--- lib/elixir_smtp.ex
+++ lib/elixir_smtp.ex
@@ -1,5 +1,5 @@
 defmodule ElixirSmtp do
   def send!(from, to, subject, body, server, login, password) do 
-    :gen_smtp_client.send({to, [from], "Subject: #{subject}\r\nFrom: #{from}\r\nTo: #{to}\r\n\r\n#{body}"}, [{:relay, server}, {:username, login}, {:password, password}])
+    :gen_smtp_client.send({from, [to], "Subject: #{subject}\r\nFrom: #{from}\r\nTo: #{to}\r\n\r\n#{body}"}, [{:relay, server}, {:username, login}, {:password, password}])
   end
 end
